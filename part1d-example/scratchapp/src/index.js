76249aa18b9b216e217f5e49df797d791c78f63d
Melissa White <melissawhite176@gmail.com>
2020-02-12 15:42:21 -0800

    add part 4 app to practice event handling

--- part1d-example/scratchapp/src/index.js
+++ part1d-example/scratchapp/src/index.js
@@ -38,7 +38,7 @@ import ReactDOM from 'react-dom';
 
 //     const handleRightClick = () => 
 //         setClicks({ ...clicks, right: clicks.right + 1 })
-    
+
 
 //     return (
 //         <div>
@@ -54,53 +54,78 @@ import ReactDOM from 'react-dom';
 
 // ReactDOM.render(<App />, document.getElementById('root'));
 
+
+
+
 //Part 3- Handling Arrays, conditional rendering
 
-const History = (props) => {
-    if (props.allClicks.legnth === 0) {
-        return (
-            <div>
-                the app is used by pressing the buttons
-            </div>
-        )
-    }
-    return (
-        <div>
-            button press history: {props.allClicks.join(' ')}
-        </div>
-    )
-}
+// const History = (props) => {
+//     if (props.allClicks.length === 0) {
+//         return (
+//             <div>
+//                 the app is used by pressing the buttons
+//             </div>
+//         )
+//     }
+//     return (
+//         <div>
+//             button press history: {props.allClicks.join(' ')}
+//         </div>
+//     )
+// }
 
-const Button = ({ onClick, text }) => (
-    <button onClick={onClick}>
-        {text}
-    </button>
-)
+// const Button = ({ onClick, text }) => (
+//     <button onClick={onClick}>
+//         {text}
+//     </button>
+// )
 
+// const App = (props) => {
+//     const [left, setLeft] = useState(0)
+//     const [right, setRight] = useState(0)
+//     const [allClicks, setAll] = useState([])
+
+//     const handleLeftClick = () => {
+//         setAll(allClicks.concat('L'))
+//         setLeft(left + 1)
+//     }
+
+//     const handleRightClick = () => {
+//         setAll(allClicks.concat('R'))
+//         setRight(right + 1)
+//     }
+
+//     return (
+//         <div>
+//             <div>
+//                 {left}
+//                 <Button onClick={handleLeftClick} text='left' />
+//                 <Button onClick={handleRightClick} text='right'/>
+//                 {right}
+//                 <History allClicks={allClicks}/>
+//             </div>
+//         </div>
+//     )
+// }
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+
+
+//Part 4 - Event Handling Revisited
 const App = (props) => {
-    const [left, setLeft] = useState(0)
-    const [right, setRIght] = useState(0)
-    const [allClicks, setAll] = useState([])
+    const [value, setValue] = useState(10)
 
-    const handleLeftClick = () => {
-        setAll(allClicks.concat('L'))
-        setLeft(left + 1)
-    }
-
-    const handleRightClick = () => {
-        setAll(allClicks.concat('R'))
-        setRIght(right + 1)
+    const setToValue = (newValue) => () => {
+        setValue(newValue)
     }
 
     return (
         <div>
-            <div>
-                {left}
-                <Button onClick={handleLeftClick} text='left' />
-                <Button onClick={handleRightClick} text='right'/>
-                {right}
-                <History allClicks={allClicks}/>
-            </div>
+            {value}
+            <button onClick={setToValue(1000)}>thousand</button>
+            <button onClick={setToValue(0)}>reset</button>
+            <button onClick={setToValue(value + 1)}>increment</button>
         </div>
     )
 }
