388a1daa872dd3ffa9eb3ee5f4603bc9cbba4f7c
Melissa White <melissawhite176@gmail.com>
2020-02-13 16:05:12 -0800

    modify app for section on passing event handlers to child components (demonstration only, app does not in this section)

--- part1d-example/scratchapp/src/index.js
+++ part1d-example/scratchapp/src/index.js
@@ -112,8 +112,13 @@ import ReactDOM from 'react-dom';
 
 
 
-//Part 4 - Event Handling Revisited
-const App = (props) => {
+//Part 5- Passing Event Handlers to Child Components
+
+const Button = (props) => (
+    <button onClick={props.handleClick}>{props.text}</button>
+)
+
+const App = () => {
     const [value, setValue] = useState(10)
 
     const setToValue = (newValue) => () => {
@@ -123,9 +128,9 @@ const App = (props) => {
     return (
         <div>
             {value}
-            <button onClick={setToValue(1000)}>thousand</button>
-            <button onClick={setToValue(0)}>reset</button>
-            <button onClick={setToValue(value + 1)}>increment</button>
+            <Button onClick={() => setToValue(1000)} text="thousand"/>
+            <Button onClick={() => setToValue(0)} text="reset"/>
+            <Button onClick={() => setToValue(value + 1)} text="increment"/>
         </div>
     )
 }
