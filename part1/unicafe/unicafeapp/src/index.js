895bd3ed9cfffc31b26cfeaab91a3c8181cf3f7b
Melissa White <melissawhite176@gmail.com>
2020-02-13 18:39:10 -0800

    1.6 unicafe step 1: create initial feedback count app

---
+++ part1/unicafe/unicafeapp/src/index.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+
+//component for display
+const DisplayGood = (props) => (
+    <div>{props.category} {props.good}</div>
+)
+const DisplayNeutral = (props) => (
+    <div>{props.category} {props.neutral}</div>
+)
+const DisplayBad = (props) => (
+    <div>{props.category} {props.bad}</div>
+)
+
+//component for two headers
+const Header = props => <h1>{props.header}</h1>
+
+//component for three buttons
+const Button = (props) => (
+    <button onClick={props.handleClick}>{props.text}</button>
+)
+
+
+
+const App = () => {
+    //save clicks of each buton to own state
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
+
+    //set new value
+    const SetToGoodValue = (newValue) => {
+        setGood(newValue)
+    }
+    const setToNeutralValue = (newValue) => {
+        setNeutral(newValue)
+    }
+
+    const setToBadValue = (newValue) => {
+        setBad(newValue)
+    }
+    console.log("new bad value", bad)
+
+    return (
+        <div>
+            <Header header="give feedback" />
+            <Button handleClick={() => SetToGoodValue(good + 1)} text="good" />
+            <Button handleClick={() => setToNeutralValue(neutral + 1)} text="neutral" />
+            <Button handleClick={() => setToBadValue(bad + 1)} text="bad" />
+            <Header header="statistics" />
+            <DisplayGood category="good" good={good} />
+            <DisplayNeutral category="neutral" neutral={neutral} />
+            <DisplayBad category="bad" bad={bad}/>
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+
