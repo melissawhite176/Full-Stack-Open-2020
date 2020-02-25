07b302b8cfa0abbe40943490fff300637c0871de
Melissa White <melissawhite176@gmail.com>
2020-02-25 12:25:16 -0800

    1.8 step 3: refactor application and create statistics component

--- part1/unicafe/unicafeapp/src/index.js
+++ part1/unicafe/unicafeapp/src/index.js
@@ -2,33 +2,42 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 //component for display
-const DisplayGood = ({category, good}) => (
+const DisplayGood = ({ category, good }) => (
     <div>{category} {good}</div>
 )
-const DisplayNeutral = ({category, neutral}) => (
+const DisplayNeutral = ({ category, neutral }) => (
     <div>{category} {neutral}</div>
 )
-const DisplayBad = ({category, bad}) => (
+const DisplayBad = ({ category, bad }) => (
     <div>{category} {bad}</div>
 )
-const DisplayTotal = ({category, total}) => (
+const DisplayTotal = ({ category, total }) => (
     <div>{category} {total}</div>
 )
-const DisplayAverage = ({category, average}) => (
-    <div>{category} {average || 0}</div>
-)
-const DisplayPositivePercent = ({category, positive}) => (
-    <div>{category} {positive || 0}%</div>
-)
+// const DisplayAverage = ({category, average}) => (
+//     <div>{category} {average || 0}</div>
+// )
+// const DisplayPositivePercent = ({category, positive}) => (
+//     <div>{category} {positive || 0}%</div>
+// )
 
 //component for two headers
-const Header = ({header}) => <h1>{header}</h1>
+const Header = ({ header }) => <h1>{header}</h1>
 
 //component for three buttons
-const Button = ({handleClick, text}) => (
+const Button = ({ handleClick, text }) => (
     <button onClick={handleClick}>{text}</button>
 )
 
+//component for statistics
+const Statistics = ({ category, statistic, text }) => {
+
+    return (
+        <div>
+            {category} {statistic || 0} {text}
+    </div>
+    )
+}
 
 
 
@@ -50,12 +59,12 @@ const App = () => {
     const setToBadValue = () => {
         setBad(bad + 1)
     }
-    
     const total = good + neutral + bad
 
-    const average = (good * 1 + neutral * 0 + bad * -1)/(good + neutral + bad)
-  
-    const positive = good/(good + neutral + bad) * 100
+    const average = (good * 1 + neutral * 0 + bad * -1) / (good + neutral + bad)
+
+    const positive = good / (good + neutral + bad) * 100 
+
 
     return (
         <div>
@@ -68,8 +77,8 @@ const App = () => {
             <DisplayNeutral category="neutral" neutral={neutral} />
             <DisplayBad category="bad" bad={bad} />
             <DisplayTotal category="all" total={total} />
-            <DisplayAverage  category="average" average={average}/>
-            <DisplayPositivePercent category="positive" positive={positive} />
+            <Statistics category="average" statistic={average} text=""/>
+            <Statistics category="positive" statistic={positive} text="%" />
         </div>
     )
 }
