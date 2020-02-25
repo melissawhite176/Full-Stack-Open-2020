beba8af058bd2218636a8aec0490ce63aeb42760
Melissa White <melissawhite176@gmail.com>
2020-02-24 23:07:19 -0800

    1.7 step 2: add statistics and destructure props

--- part1/unicafe/unicafeapp/src/index.js
+++ part1/unicafe/unicafeapp/src/index.js
@@ -2,38 +2,60 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 //component for display
-const DisplayGood = (props) => (
-    <div>{props.category} {props.good}</div>
+const DisplayGood = ({category, good}) => (
+    <div>{category} {good}</div>
 )
-const DisplayNeutral = (props) => (
-    <div>{props.category} {props.neutral}</div>
+const DisplayNeutral = ({category, neutral}) => (
+    <div>{category} {neutral}</div>
 )
-const DisplayBad = (props) => (
-    <div>{props.category} {props.bad}</div>
+const DisplayBad = ({category, bad}) => (
+    <div>{category} {bad}</div>
+)
+const DisplayTotal = ({category, total}) => (
+    <div>{category} {total}</div>
+)
+const DisplayAverage = ({category, average}) => (
+    <div>{category} {average || 0}</div>
+)
+const DisplayPositivePercent = ({category, positive}) => (
+    <div>{category} {positive || 0}%</div>
 )
 
 //component for two headers
-const Header = props => <h1>{props.header}</h1>
+const Header = ({header}) => <h1>{header}</h1>
 
 //component for three buttons
-const Button = (props) => (
-    <button onClick={props.handleClick}>{props.text}</button>
+const Button = ({handleClick, text}) => (
+    <button onClick={handleClick}>{text}</button>
 )
+
 
 
 
 const App = () => {
-    //save clicks of each buton to own state
+    //save clicks of each button to own state
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
 
     //set new value
-    const setToGoodValue = () => setGood(good + 1)
+    const setToGoodValue = () => {
+        setGood(good + 1)
+    }
 
-    const setToNeutralValue = () => setNeutral(neutral + 1)
+    const setToNeutralValue = () => {
+        setNeutral(neutral + 1)
+    }
 
-    const setToBadValue = () => setBad(bad + 1)
+    const setToBadValue = () => {
+        setBad(bad + 1)
+    }
+    
+    const total = good + neutral + bad
+
+    const average = (good * 1 + neutral * 0 + bad * -1)/(good + neutral + bad)
+  
+    const positive = good/(good + neutral + bad) * 100
 
     return (
         <div>
@@ -45,6 +67,9 @@ const App = () => {
             <DisplayGood category="good" good={good} />
             <DisplayNeutral category="neutral" neutral={neutral} />
             <DisplayBad category="bad" bad={bad} />
+            <DisplayTotal category="all" total={total} />
+            <DisplayAverage  category="average" average={average}/>
+            <DisplayPositivePercent category="positive" positive={positive} />
         </div>
     )
 }
