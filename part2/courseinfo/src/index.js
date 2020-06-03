13a567c1db1da09f962f29bf84d6ce5f375847cb
Melissa White <melissawhite176@gmail.com>
2020-06-03 12:57:13 -0700

    2.4: Course contents step9

--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -23,9 +23,31 @@ const App = () => {
           exercises: 14,
           id: 3
         },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
       ]
     }
   ]
+
   return (
     <div>
       {courses.map(course => <Course key={course.id} course={course} />)}
