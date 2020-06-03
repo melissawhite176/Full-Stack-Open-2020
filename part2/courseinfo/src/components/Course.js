88f7eb9d426658465d630c39de9f724770315109
Melissa White <melissawhite176@gmail.com>
2020-06-03 12:52:20 -0700

    2.2: Course contents step7

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -16,12 +16,18 @@ const Content = ({ course }) => course.parts.map(part =>
     <Part key={part.id} part={part.name} exercises={part.exercises} />
 )
 
+const Total = ({ course }) => (
+    <p>
+        <strong>total of {course.parts.reduce((sum, a) => sum + a.exercises, 0)} exercises</strong>
+    </p>
+)
 
 const Course = ({ course }) => {
     return (
         <>
             <Header name={course.name} />
             <Content course={course} />
+            <Total course={course} />
         </>
     )
 }
