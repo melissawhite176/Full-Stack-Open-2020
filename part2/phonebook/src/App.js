1a861889a45890adea0579f0c131de8d34eaf6e6
Melissa White <melissawhite176@gmail.com>
2020-06-12 15:29:25 -0700

    phonebook fetch data from server (step 6)

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,18 +1,22 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
+import axios from 'axios'
 
-const App = (props) => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '555-555-5555' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+const App = () => {
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('a new name...')
   const [newNumber, setNewNumber] = useState('555-555-5555')
   const [search, setSearch] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
 
   const addName = (event) => {
     event.preventDefault()
