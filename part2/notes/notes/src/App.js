83e4a04499c0b7bef35346a16a2d09b32406d0a7
Melissa White <melissawhite176@gmail.com>
2020-06-15 11:01:56 -0700

    extracting communication with the backend into a separate module (part2d)

--- part2/notes/notes/src/App.js
+++ part2/notes/notes/src/App.js
@@ -1,35 +1,33 @@
 import React, { useState, useEffect } from 'react'
-import axios from 'axios'
 import Note from './components/Note'
+import noteService from './services/notes'
 
 const App = (props) => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
 
+  useEffect(() => {
+    noteService
+      .getAll()
+      .then(initialNotes => {
+        setNotes(initialNotes)
+      })
+  }, [])
+
   const toggleImportanceOf = (id) => {
-    const url = `http://localhost:3001/notes/${id}`
+    console.log(`importance changed on note id:  ${id}`)
     const note = notes.find(n => n.id === id)
     const changedNote = { ...note, important: !note.important }
 
-    console.log('object spread', {...note})
-    console.log('changedNote ', changedNote)
-    
-    axios.put(url, changedNote).then(response => {
-      setNotes(notes.map(note => note.id !== id ? note : response.data))
+    noteService
+    .update(id, changedNote)
+    .then(returnedNote => {
+      setNotes(notes.map(note => note.id !== id ? note : returnedNote))
     })
   }
 
-  useEffect(() => {
-    console.log('effect')
-    axios
-      .get('http://localhost:3001/notes')
-      .then(response => {
-        console.log('promise fulfilled')
-        setNotes(response.data)
-      })
-  }, [])
-  console.log('render', notes.length, 'notes')
+
 
   const notesToShow = showAll
     ? notes
@@ -43,17 +41,15 @@ const App = (props) => {
       important: Math.random() > 0.5,
     }
 
-    axios
-      .post('http://localhost:3001/notes', noteObject)
-      .then(response => {
-        console.log(response)
-        setNotes(notes.concat(response.data))
+    noteService
+    .create(noteObject)
+      .then(returnedNote => {
+        setNotes(notes.concat(returnedNote))
         setNewNote('')
       })
   }
 
   const handleNoteChange = (event) => {
-    console.log(event.target.value)
     setNewNote(event.target.value)
   }
 
---
+++ part2/notes/notes/src/services/notes.js
@@ -0,0 +1,27 @@
+import axios from 'axios'
+const baseUrl = 'http://localhost:3001/notes'
+
+const getAll = () => {
+    console.log('get all promise returned')
+    const request = axios.get(baseUrl)
+    return request.then(response => response.data)
+}
+
+const create = newObject => {
+    console.log('create promise returned')
+    const request = axios.post(baseUrl, newObject)
+    return request.then(response => response.data)
+}
+
+const update = (id, newObject) => {
+    console.log('update promise returned')
+    const request =  axios.put(`${baseUrl}/${id}`, newObject)
+    return request.then(response => response.data)
+
+}
+
+export default {
+    getAll: getAll,
+    create: create,
+    update: update
+}
