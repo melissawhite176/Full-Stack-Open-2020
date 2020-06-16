77391f6dc7636d241907cb2bbcf97402c9b86f4d
Melissa White <melissawhite176@gmail.com>
2020-06-16 12:40:00 -0700

    install nodemon dependency (part 3a)

--- scratch apps/part3-backend/backend/index.js
+++ scratch apps/part3-backend/backend/index.js
@@ -1,4 +1,5 @@
-const http = require('http')
+const express = require('express')
+const app = express()
 
 let notes = [
   {
@@ -18,11 +19,27 @@ let notes = [
     content: "GET and POST are the most important methods of HTTP protocol",
     date: "2019-05-30T19:20:14.298Z",
     important: true
+  },
+  {
+    id: 4,
+    content: "PUT and DELETE are also important methods of HTTP protocol",
+    date: "2019-05-30T19:55:14.298Z",
+    important: true
+  },
+  {
+    id: 5,
+    content: "Node and Express are also easy",
+    date: "2019-05-30T19:57:14.298Z",
+    important: true
   }
 ]
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'application/json' })
-  response.end(JSON.stringify(notes))
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/notes', (req, res) => {
+  res.json(notes)
 })
 
 const port = 3001
