0d3b6a4a41dbfaef92eaba43b6961a923b128a14
Melissa White <melissawhite176@gmail.com>
2020-06-16 22:24:44 -0700

    display single entry by id- phonebook backend step 3

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -37,9 +37,19 @@ app.get('/info', (req, res) => {
 
 })
 
-
 app.get('/api/persons', (req, res) => {
     res.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
 })
 
 
