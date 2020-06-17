9553ccdcac386bcc10213d343ba971b6b985408b
Melissa White <melissawhite176@gmail.com>
2020-06-16 21:39:09 -0700

    initial phonebook node application - phonebook backend step 1

---
+++ part3/phonebook-backend/index.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const app = express()
+
+const persons = [
+    {
+        name: "Arto Hellas",
+        number: "040-123456",
+        id: 1
+    },
+    {
+        name: "Ada Lovelace",
+        number: "39-44-543235",
+        id: 2
+    },
+    {
+        name: "Dan Abramov",
+        number: "12-43-234234",
+        id: 3
+    },
+    {
+        name: "Mary Poppendieck",
+        number: "39-23-632122",
+        id: 4
+    }
+]
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World!</h1>')
+  })
+  
+  app.get('/api/persons', (req, res) => {
+    res.json(persons)
+  })
+
+const port = 3001
+app.listen(port)
+console.log(`Server running on port ${port}`)
