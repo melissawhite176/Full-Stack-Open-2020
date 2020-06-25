b853f6fb818299700e7f6109e5a2c6dacc1aa742
Melissa White <melissawhite176@gmail.com>
2020-06-24 19:15:08 -0700

    add cors require

--- scratch apps/part3-backend/backend/index.js
+++ scratch apps/part3-backend/backend/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
 app.use(express.json())
+app.use(cors())
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
@@ -72,6 +74,7 @@ app.post('/api/notes', (request, response) => {
   notes = notes.concat(note)
 
   response.json(note)
+  
 })
 
 app.get('/api/notes/:id', (request, response) => {
@@ -93,7 +96,7 @@ app.delete('/api/notes/:id', (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
