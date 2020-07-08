feb0d400bd0f62b9da67fba2c93fe1d8a003a6a2
Melissa White <melissawhite176@gmail.com>
2020-07-07 18:53:27 -0700

    show error message on create - phonebook (3.20)

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -75,6 +75,12 @@ const App = () => {
           setNotification(null)
         }, 5000)
       })
+      .catch(error => {
+        setErrorMessage(`error: ${error.response.data.error}`)
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
+      })
   }
 
   const shownNames = search.length > 0
