4a369f067ce9dfa17efbb3f91ff0424df319623b
Melissa White <melissawhite176@gmail.com>
2020-07-13 14:31:58 -0700

    add hardcoded list of blogs, modify server content type and response to JSON format - bloglist backend

--- part4/bloglist-backend/index.js
+++ part4/bloglist-backend/index.js
@@ -1,8 +1,32 @@
 const http = require('http')
 
-const app = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' })
-  res.end('Hello World')
+ let blogs = [
+  {
+    id: 1,
+    title: "Delicious Desserts Blog",
+    author: "Joanna Goodman",
+    url: "https://www.google.com",
+    likes: 4
+  },
+  {
+    id: 2,
+    title: "Tasty Treats Blog",
+    author: "Norton Oswald",
+    url: "https://www.wikipedia.com",
+    likes: 6
+  },
+  {
+    id: 3,
+    title: "DIY Furniture Blog",
+    author: "Virginia Woolf",
+    url: "https://www.fullstackopen.com",
+    likes: 10
+  }
+ ]
+
+const app = http.createServer((request, response) => {
+  response.writeHead(200, { 'Content-Type': 'application/json' })
+  response.end(JSON.stringify(blogs))
 })
 
 const port = 3001
