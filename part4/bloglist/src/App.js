89252fe83e0b962a6280d4062ff1787da0f3838a
Melissa White <melissawhite176@gmail.com>
2020-07-13 13:45:42 -0700

    modify app component to render blogs from server - bloglist

--- part4/bloglist/src/App.js
+++ part4/bloglist/src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
+import Blog from './components/Blog'
+import axios from 'axios'
 
-function App() {
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  
+  useEffect(() => {
+    console.log('effect')
+    axios 
+      .get('http://localhost:3001/blogs')
+      .then(response => {
+        console.log('promise fulfilled')
+        setBlogs(response.data)
+      })
+  }, [])
+  console.log('render', blogs.length, 'blogs')
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Blogs</h1>
+      <ul>
+        {blogs.map(blog =>
+          <Blog key={blog.id} blog={blog} />
+        )}
+      </ul>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
