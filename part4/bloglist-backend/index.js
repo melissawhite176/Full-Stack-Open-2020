5e22861473793bf3362126ab9b9a2f58f07241e3
Melissa White <melissawhite176@gmail.com>
2020-07-13 14:24:37 -0700

    create simple web server- bloglist backend

--- part4/bloglist-backend/index.js
+++ part4/bloglist-backend/index.js
@@ -1,1 +1,10 @@
-console.log('hello world')
+const http = require('http')
+
+const app = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' })
+  res.end('Hello World')
+})
+
+const port = 3001
+app.listen(port)
+console.log(`Server running on port ${port}`)
